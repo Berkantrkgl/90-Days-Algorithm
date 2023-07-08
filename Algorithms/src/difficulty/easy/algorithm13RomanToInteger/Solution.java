ce864dee3f614a5dd8740f0c9953fdfa6d575fee
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2023-07-09 00:16:50 +0300

    Add another good solution for algorithm 13

--- Algorithms/src/difficulty/easy/algorithm13RomanToInteger/Solution.java
+++ Algorithms/src/difficulty/easy/algorithm13RomanToInteger/Solution.java
@@ -1,10 +1,13 @@
 package difficulty.easy.algorithm13RomanToInteger;
 
+import java.util.HashMap;
+
 public class Solution {
 
     public static void main(String[] args) {
         String s = "MCMXCIV";
         System.out.println(romanToInt(s));
+        System.out.println(anotherRomanToInteger(s));
     }
 
     // This is my own solution =>
@@ -67,6 +70,32 @@ public class Solution {
 
             }
         }
+
+        return result;
+    }
+
+    // Another good solution
+    public static int anotherRomanToInteger(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = map.get(s.charAt(s.length()-1));
+        for (int i = s.length() - 2 ; i >= 0 ; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            }
+            else result += map.get(s.charAt(i));
+        }
+
+
 
         return result;
     }
