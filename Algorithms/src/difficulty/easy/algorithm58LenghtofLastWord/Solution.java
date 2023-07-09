607222a73788d577e899f14e4fd64d7c2b4920b5
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2023-07-09 12:57:02 +0300

    Add another solution for algorithm 58

--- Algorithms/src/difficulty/easy/algorithm58LenghtofLastWord/Solution.java
+++ Algorithms/src/difficulty/easy/algorithm58LenghtofLastWord/Solution.java
@@ -8,10 +8,12 @@ public class Solution {
         String s = "a ";
         System.out.println(lengthOfLastWord(s));
 
+        System.out.println(anotherLengthOfLastWord(s));
 
 
     }
 
+    // This is my own solution
     public static int lengthOfLastWord(String s) {
         int sum = 0;
         if (s.length() == 0) {
@@ -34,5 +36,19 @@ public class Solution {
            }
         }
         return sum;
+    }
+
+    // This is another solution which is very simple and good.
+    public static int anotherLengthOfLastWord(String s) {
+        int length = 0;
+
+        for (int i = s.length() - 1 ; i >= 0 ; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                if (length > 0 ) return length;
+            }
+        }
+        return length;
     }
 }
