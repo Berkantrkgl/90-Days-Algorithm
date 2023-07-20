08c92fb4683d4e2fdbcfa428b7c58e8745381faa
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2023-07-20 20:57:16 +0300

    Tried to solve ListNode algorithm

---
+++ Algorithms/src/difficulty/easy/algorithm141/Solution141.java
@@ -0,0 +1,23 @@
+package difficulty.easy.algorithm141;
+
+public class Solution141 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+
+        return true;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
