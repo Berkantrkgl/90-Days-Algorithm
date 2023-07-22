ba0974af9005c4d5c929680f4e2c7887b5eae710
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2023-07-22 12:42:26 +0300

    Solved algorithm 141

--- Algorithms/src/difficulty/easy/algorithm141/Solution141.java
+++ Algorithms/src/difficulty/easy/algorithm141/Solution141.java
@@ -1,15 +1,24 @@
 package difficulty.easy.algorithm141;
 
+import java.util.LinkedList;
+
 public class Solution141 {
 
     public static void main(String[] args) {
-        
+        LinkedList<Integer> linkedList = new LinkedList<>();
     }
 
     public static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
 
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
 
-        return true;
+            if (fast == slow) return true;
+        }
+        return false;
     }
 }
 
