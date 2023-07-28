d43029dd06a62248733d307af4625082bd6bf5ae
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-07-28 08:41:33 +0300

    Updated algorithm 69

--- Algorithms/src/difficulty/easy/algorithm69/Solution69.java
+++ Algorithms/src/difficulty/easy/algorithm69/Solution69.java
@@ -10,22 +10,21 @@ public class Solution69 {
 
         if (x < 0) return 0;
 
-        int start = 0;
+        int start = 1;
         int end = x;
         int res = 0;
 
         while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (mid * mid < x) {
-                start = mid + 1;
-                res = mid;
-            } else if (mid * mid > x) {
+            int mid = (start + end) / 2;
+            // Burada mid * mid yapmak yerine mid > x / mid yapmak gerek.
+            // Cunku leetcode ile yapilan testlerde saklama alanai sorunu yasaniyor.
+            if (mid > x / mid) {
                 end = mid - 1;
             } else {
-                return mid;
+                start = mid + 1;
+                res = mid;
             }
         }
-
         return res;
     }
 }
