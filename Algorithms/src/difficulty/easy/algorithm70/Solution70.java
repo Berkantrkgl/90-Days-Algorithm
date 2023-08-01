9b28b6c6fd617f80cd15290ed5bc21df31120ecb
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-08-01 09:59:52 +0300

    Solved algorithm 70

---
+++ Algorithms/src/difficulty/easy/algorithm70/Solution70.java
@@ -0,0 +1,20 @@
+package difficulty.easy.algorithm70;
+
+public class Solution70 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(climbStairs(n));
+    }
+
+    public static int climbStairs(int n) {
+        if(n == 0 || n == 1 || n == 2){return n;}
+        int[] mem = new int[n];
+        mem[0] = 1;
+        mem[1] = 2;
+        for(int i = 2; i < n; i++){
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+        return mem[n-1];
+    }
+}
