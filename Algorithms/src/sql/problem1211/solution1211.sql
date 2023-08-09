d2fd27f9f4408c2563880ce1834e62578137e26e
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-08-09 09:17:25 +0300

    Updated algorithm 1211

--- Algorithms/src/sql/problem1211/solution1211.sql
+++ Algorithms/src/sql/problem1211/solution1211.sql
@@ -29,4 +29,3 @@ SELECT query_name, ROUND(AVG(rating / position), 2) AS quality,
         ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END ) / COUNT(*) * 100 , 2) as poor_query_percentage
         FROM Queries
         GROUP BY query_name;
-        
